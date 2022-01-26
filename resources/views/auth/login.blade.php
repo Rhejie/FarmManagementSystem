b4d1fff7c550dc4592e7fb9330b794c6beed6579
rhejiegitlab <aranezrhejie@gmail.com>
2022-01-26 23:12:28 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="hold-transition login-page">
+{{-- <div class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
             <a href="../../index2.html"><b>Farm</b> Management System</a>
@@ -47,8 +47,8 @@
             <!-- /.login-card-body -->
         </div>
     </div>
-</div>
-{{-- <div class="container">
+</div> --}}
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -103,12 +103,6 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
@@ -116,5 +110,5 @@
             </div>
         </div>
     </div>
-</div> --}}
+</div>
 @endsection
